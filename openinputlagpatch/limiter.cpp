bafeccc8c8488430f304ccb5fd3c46ad784aa27f
Khang <khang06@users.noreply.github.com>
2022-06-27 13:54:23 -0400

    Export an API for other mods

--- openinputlagpatch/limiter.cpp
+++ openinputlagpatch/limiter.cpp
@@ -5,28 +5,33 @@
 #include <stdio.h>
 #include "d3d9_hook.h"
 
+bool limiter_initialized = false;
 LARGE_INTEGER limiter_start;
 unsigned int frame_num;
 LARGE_INTEGER wait_amount;
 LARGE_INTEGER blt_prepare_time;
+LARGE_INTEGER perf_freq;
+DWORD queued_wait_amount = 0;
 
 void limiter_init() {
-	LARGE_INTEGER perf_freq;
 	QueryPerformanceFrequency(&perf_freq);
 	QueryPerformanceCounter(&limiter_start);
 	wait_amount.QuadPart = (LONGLONG)((double)perf_freq.QuadPart / 60.0);
+	queued_wait_amount = wait_amount.LowPart;
 	blt_prepare_time.QuadPart = perf_freq.QuadPart / 1000 * 2; // 2 ms
+	limiter_initialized = true;
 }
 
 void limiter_tick() {
 	__int64 target = limiter_start.QuadPart + ++frame_num * wait_amount.QuadPart;
 	LARGE_INTEGER cur_time;
 	QueryPerformanceCounter(&cur_time);
-	if (target - blt_prepare_time.QuadPart >= cur_time.QuadPart) {
+	if (target - blt_prepare_time.QuadPart >= cur_time.QuadPart && queued_wait_amount == wait_amount.LowPart) {
 		while (cur_time.QuadPart < target)
 			QueryPerformanceCounter(&cur_time);
 	} else {
-		printf("Frame limiter fell behind!!! Resyncing...\n");
+		printf("Frame limiter fell behind or target FPS has changed. Resyncing...\n");
+		wait_amount.LowPart = queued_wait_amount;
 		if (d3d9ex_device)
 			d3d9ex_device->WaitForVBlank(0);
 		QueryPerformanceCounter(&cur_time);
--- openinputlagpatch/limiter.h
+++ openinputlagpatch/limiter.h
@@ -1,4 +1,10 @@
 #pragma once
 
+#include <Windows.h>
+
+extern bool limiter_initialized;
+extern LARGE_INTEGER perf_freq;
+extern DWORD queued_wait_amount;
+
 void limiter_init();
 void limiter_tick();
