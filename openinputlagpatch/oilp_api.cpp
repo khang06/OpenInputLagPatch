90096582e2f99ec7d3c0167695b85b352b7cd0ab
32th-System <intensonat@gmail.com>
2022-06-29 11:29:55 +0200

    API: specify calling convention and add API to get FPS

--- openinputlagpatch/oilp_api.cpp
+++ openinputlagpatch/oilp_api.cpp
@@ -1,10 +1,15 @@
 // Exported functions to allow compatibility with external tools such as thprac
 
 #include <Windows.h>
+#include "config.h"
 #include "limiter.h"
 
 #define OILP_API extern "C" __declspec(dllexport)
 
-OILP_API bool oilp_set_game_fps(int fps) {
+OILP_API bool __stdcall oilp_set_game_fps(int fps) {
 	return Limiter::SetGameFPS(fps);
+}
+
+OILP_API int __stdcall oilp_get_game_fps() {
+	return Config::GameFPS;
 }
