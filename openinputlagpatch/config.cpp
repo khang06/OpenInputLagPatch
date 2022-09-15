e1e38a319aa6fb30b27b4bd0bf1a10ae019b9692
Khang <khang06@users.noreply.github.com>
2022-09-15 00:09:55 -0400

    Make config loading less painful to look at

--- openinputlagpatch/config.cpp
+++ openinputlagpatch/config.cpp
@@ -2,6 +2,7 @@
 #include <shlwapi.h>
 #include "config.h"
 
+// Default config values
 UINT Config::GameFPS = 60;
 BOOL Config::ReplaySpeedControl = TRUE;
 UINT Config::ReplaySkipFPS = 240;
@@ -16,6 +17,9 @@ BOOL Config::DebugWait = FALSE;
 BOOL Config::FixInputGlitching = FALSE;
 TouhouGame Config::GameOverride = TouhouGame::Unknown;
 
+// Helper macro for loading a specific setting value
+#define LOAD_SETTING(x) Config::x = (decltype(Config::x))GetPrivateProfileInt(TEXT("Option"), TEXT(#x), (int)Config::x, config_path)
+
 bool Config::Load() {
 	// Get the config file path
 	wchar_t config_path[1024] = {};
@@ -25,19 +29,19 @@ bool Config::Load() {
 	PathAppendW(config_path, L"\\openinputlagpatch.ini");
 
 	// Load the config
-	Config::GameFPS = GetPrivateProfileInt(L"Option", L"GameFPS", Config::GameFPS, config_path);
-	Config::ReplaySpeedControl = GetPrivateProfileInt(L"Option", L"ReplaySpeedControl", Config::ReplaySpeedControl, config_path);
-	Config::ReplaySkipFPS = GetPrivateProfileInt(L"Option", L"ReplaySkipFPS", Config::ReplaySkipFPS, config_path);
-	Config::ReplaySlowFPS = GetPrivateProfileInt(L"Option", L"ReplaySlowFPS", Config::ReplaySlowFPS, config_path);
-	Config::BltPrepareTime = GetPrivateProfileInt(L"Option", L"BltPrepareTime", Config::BltPrepareTime, config_path);
-	Config::Sleep = (SleepType)GetPrivateProfileInt(L"Option", L"Sleep", Config::Sleep, config_path);
-	Config::D3D9Ex = GetPrivateProfileInt(L"Option", L"D3D9Ex", Config::D3D9Ex, config_path);
-	Config::FullscreenRefreshRate = (TargetRefreshRate)GetPrivateProfileInt(L"Option", L"FullscreenRefreshRate", Config::FullscreenRefreshRate, config_path);
-	Config::ShowOverlay = GetPrivateProfileInt(L"Option", L"ShowOverlay", Config::ShowOverlay, config_path);
-	Config::DebugConsole = GetPrivateProfileInt(L"Option", L"DebugConsole", Config::DebugConsole, config_path);
-	Config::DebugWait = GetPrivateProfileInt(L"Option", L"DebugWait", Config::DebugWait, config_path);
-	Config::FixInputGlitching = GetPrivateProfileInt(L"Option", L"FixInputGlitching", Config::FixInputGlitching, config_path);
-	Config::GameOverride = (TouhouGame)GetPrivateProfileInt(L"Option", L"GameOverride", (int)Config::GameOverride, config_path);
+	LOAD_SETTING(GameFPS);
+	LOAD_SETTING(ReplaySpeedControl);
+	LOAD_SETTING(ReplaySkipFPS);
+	LOAD_SETTING(ReplaySlowFPS);
+	LOAD_SETTING(BltPrepareTime);
+	LOAD_SETTING(Sleep);
+	LOAD_SETTING(D3D9Ex);
+	LOAD_SETTING(FullscreenRefreshRate);
+	LOAD_SETTING(ShowOverlay);
+	LOAD_SETTING(DebugConsole);
+	LOAD_SETTING(DebugWait);
+	LOAD_SETTING(FixInputGlitching);
+	LOAD_SETTING(GameOverride);
 
 	// Validate options
 	if (Config::GameFPS < 60)
@@ -56,3 +60,5 @@ bool Config::Load() {
 
 	return true;
 }
+
+#undef LOAD_SETTING
