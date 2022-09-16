19965fc8b8cbe0a244a73cbf7656fd44c764351b
32th-System <intensonat@gmail.com>
2022-09-16 20:46:20 +0200

    Functions in the API don't need extern "C"

--- openinputlagpatch/oilp_api.cpp
+++ openinputlagpatch/oilp_api.cpp
@@ -4,30 +4,30 @@
 #include "config.h"
 #include "limiter.h"
 
-extern "C" {
-	bool __stdcall oilp_set_game_fps(int fps) {
-		return Limiter::SetGameFPS(fps);
-	}
+bool __stdcall oilp_set_game_fps(int fps) {
+	return Limiter::SetGameFPS(fps);
+}
 
-	int __stdcall oilp_get_game_fps() {
-		return Config::GameFPS;
-	}
+bool __stdcall oilp_set_replay_skip_fps(int fps) {
+	Config::ReplaySkipFPS = fps;
+	return true;
+}
 
-	bool __stdcall oilp_set_replay_skip_fps(int fps) {
-		Config::ReplaySkipFPS = fps;
-		return true;
-	}
+bool __stdcall oilp_set_replay_slow_fps(int fps) {
+	Config::ReplaySlowFPS = fps;
+	return true;
+}
 
-	int __stdcall oilp_get_replay_skip_fps() {
-		return Config::ReplaySkipFPS;
-	}
+int oilp_get_game_fps() {
+	return Config::GameFPS;
+}
 
-	bool __stdcall oilp_set_replay_slow_fps(int fps) {
-		Config::ReplaySlowFPS = fps;
-		return true;
-	}
 
-	int __stdcall oilp_get_replay_slow_fps() {
-		return Config::ReplaySlowFPS;
-	}
+int oilp_get_replay_skip_fps() {
+	return Config::ReplaySkipFPS;
+}
+
+
+int oilp_get_replay_slow_fps() {
+	return Config::ReplaySlowFPS;
 }
