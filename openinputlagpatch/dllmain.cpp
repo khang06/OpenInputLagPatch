3127021bf3fa8289f6cbaf40a10f6dd93e9d836f
32th-System <intensonat@gmail.com>
2022-09-16 20:35:00 +0200

    Export with .def
    This will also export the function with a plain non mangled name

--- openinputlagpatch/dllmain.cpp
+++ openinputlagpatch/dllmain.cpp
@@ -193,10 +193,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
-// Fake export to allow the dll to be loaded as dinput8.dll
-// NOTE: "/EXPORT:DirectInput8Create=_DirectInput8Create@20" must be added to the linker options
-// Otherwise, this will be exported as _DirectInput8Create@20 instead of DirectInput8Create
-extern "C" __declspec(dllexport) HRESULT WINAPI DirectInput8Create(HINSTANCE hinst, DWORD dwVersion,
+HRESULT WINAPI DirectInput8Create(HINSTANCE hinst, DWORD dwVersion,
     REFIID riidltf, LPVOID* ppvOut, LPUNKNOWN punkOuter)
 {
     wchar_t real_dinput8_path[MAX_PATH] = {};
